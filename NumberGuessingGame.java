9604a2669b608967ce0981a11cbe778775e8c67d
bhavanidayala1-coder <bhavanidayala1@gmail.com>
2026-06-22 20:25:44 +0530

    Implement number guessing game logic

--- NumberGuessingGame.java
+++ NumberGuessingGame.java
@@ -1,1 +1,77 @@
 
+import java.util.Random;
+import java.util.Scanner;
+
+public class NumberGuessingGame
+{
+public static void main(String[] args)
+{
+
+Random random = new Random();
+Scanner sc = new Scanner(System.in);
+
+int number = random.nextInt(100)+1;
+int guess;
+
+System.out.println("Guess a number between 1 and 100");
+do
+{
+System.out.print("Enter your guess: "); 
+guess = sc.nextInt();
+
+if(guess > number)
+{
+System.out.println("Too high!");
+}
+else if(guess < number)import java.util.Random;
+import java.util.Scanner;
+
+public class NumberGuessingGame
+{
+public static void main(String[] args)
+{
+
+Random random = new Random();
+Scanner sc = new Scanner(System.in);
+
+int number = random.nextInt(100)+1;
+int guess;
+
+System.out.println("Guess a number between 1 and 100");
+do
+{
+System.out.print("Enter your guess: "); 
+guess = sc.nextInt();
+
+if(guess > number)
+{
+System.out.println("Too high!");
+}
+else if(guess < number)
+{
+System.out.println("Too low!");
+}
+else
+{
+System.out.println("Congratulations! you guessed the number . ");
+}
+}
+while(guess != number);
+sc.close();
+}
+}
+
+
+{
+System.out.println("Too low!");
+}
+else
+{
+System.out.println("Congratulations! you guessed the number . ");
+}
+}
+while(guess != number);
+sc.close();
+}
+}
+
